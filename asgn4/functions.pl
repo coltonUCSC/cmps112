dd863666ca5a0b814f4d16540b1874d27041e610
coltonUCSC <colton.willey@gmail.com>
2017-06-04 19:28:42 -0700

    initial working version with most error checking handled

--- asgn4/functions.pl
+++ asgn4/functions.pl
@@ -1,10 +1,7 @@
 % $Id: functions.pl,v 1.3 2016-11-08 15:04:13-08 - - $
 
-mathfns( X, List ) :-
-   S is sin( X ),
-   C is cos( X ),
-   Q is sqrt( X ),
-   List = [S, C, Q].
+not(X) :- X, !, fail.
+not(_).
 
 constants( List ) :-
    Pi is pi,
@@ -12,14 +9,17 @@ constants( List ) :-
    Epsilon is epsilon,
    List = [Pi, E, Epsilon].
 
+sincos( X, Y ) :-
+   Y is sin( X ) ** 2 + cos( X ) ** 2.
+
 dms_to_dec(degmin(Degree, Minutes), Decimal) :-
    Decimal is Degree + (Minutes / 60).
 
 degree_to_radians(Degree, Radians) :-
    Radians is ((Degree * pi) / (180)).
 
-sincos( X, Y ) :-
-   Y is sin( X ) ** 2 + cos( X ) ** 2.
+hm_to_h(time(H, M), Hours) :-
+    Hours is H + M / 60.
 
 haversine_radians( Lat1, Lon1, Lat2, Lon2, Distance ) :-
    Dlon is Lon2 - Lon1,
@@ -29,9 +29,9 @@ haversine_radians( Lat1, Lon1, Lat2, Lon2, Distance ) :-
    Dist is 2 * atan2( sqrt( A ), sqrt( 1 - A )),
    Distance is Dist * 3961.
 
-compute_distance(Airport1, Airport2, Distance) :-
-   airport(Airport1, _, Lat1, Lon1),
-   airport(Airport2, _, Lat2, Lon2),
+compute_distance(Src, Dest, Distance) :-
+   airport(Src, _, Lat1, Lon1),
+   airport(Dest, _, Lat2, Lon2),
    dms_to_dec(Lat1, Lat1_dec),
    dms_to_dec(Lon1, Lon1_dec),
    dms_to_dec(Lat2, Lat2_dec),
@@ -42,8 +42,85 @@ compute_distance(Airport1, Airport2, Distance) :-
    degree_to_radians(Lon2_dec, Lon2_rad),
    haversine_radians(Lat1_rad, Lon1_rad, Lat2_rad, Lon2_rad, Distance).
 
-get_distance(Airport1, Airport2) :-
-   compute_distance(Airport1, Airport2, Distance),
-   write('Distance: '),
-   write(Distance),
-   nl.
+compute_arrival_time(flight(Src, Dest, DepartTime), ArriveTime) :-
+   compute_distance(Src, Dest, Distance),
+   FlightTime is Distance / 500,
+   hm_to_h(DepartTime, DepartTime_hours),
+   ArriveTime is DepartTime_hours + FlightTime.
+
+display_num_with_padding(Num) :-
+   Num < 10, 
+   write(0), 
+   write(Num).
+
+display_num_with_padding(Num) :-
+   Num >= 10, 
+   write(Num).
+
+display_time(HO) :-
+   MO is floor(HO * 60),
+   H is MO // 60,
+   M is MO mod 60,
+   display_num_with_padding(H), 
+   write(':'), 
+   display_num_with_padding(M).
+
+display_path([]).
+display_path([flight(Depart, Arrive, DTime)|Path]) :-
+    airport(Depart, DName, _, _), airport(Arrive, AName, _, _),
+    write('depart  '), write(Depart), write('  '), write(DName),
+    write('  '), hm_to_h(DTime, DepartT), display_time(DepartT), nl,
+    write('arrive  '), write(Arrive), write('  '), write(AName),
+    write('  '), compute_arrival_time(flight(Depart, Arrive, DTime), ArriveT),
+    display_time(ArriveT), nl, !,
+    display_path(Path).
+display_path(_).
+
+valid_flight(DepartTime, ArriveTime, PrevArriveTime) :-
+   hm_to_h(DepartTime, DepartTime_hours),
+   (ArriveTime < 24),
+   (DepartTime_hours > PrevArriveTime),
+   ((DepartTime_hours - PrevArriveTime) > 0.5).
+
+find_path(Src, Final, [flight(Src, Dest, DepartTime)|Path]) :-
+   flight(Src, Dest, DepartTime),
+   find_path(Dest, Final, [flight(Src, Dest, DepartTime)], Path).
+
+find_path(Src, Src, _, [Src]).
+find_path(Src, Final, [flight(PrevSrc, PrevDest, PrevDepartTime)|Tried], [flight(Src, Dest, DepartTime)|Path]) :-
+   flight(Src, Dest, DepartTime),
+   compute_arrival_time(flight(Src, Dest, DepartTime), ArriveTime),
+   not(member(flight(Src, Dest, DepartTime), Tried)),
+   compute_arrival_time(flight(PrevSrc, PrevDest, PrevDepartTime), PrevArriveTime),
+   valid_flight(DepartTime, ArriveTime, PrevArriveTime),
+   find_path(Dest, Final, [flight(Src, Dest, DepartTime)|Tried], Path).
+
+fly(Src, Src) :-
+    write('Source and desitination airports can not be equal'),
+    nl, !, fail.
+
+fly(Src, Dest) :-
+   airport(Src, _, _, _),
+   airport(Dest, _, _, _),
+   find_path(Src, Dest, Path), nl,
+   display_path(Path), !.
+
+fly(Src, Dest) :-
+   airport(Src, _, _, _),
+   airport(Dest, _, _, _),
+   write('No valid path exists'), nl, !, fail.
+
+fly(Src, Dest) :-
+    not(airport(Src, _, _, _)),
+    airport(Dest, _, _, _),
+    write('Source airport does not exist'), nl, !, fail.
+
+fly(Src, Dest) :-
+    airport(Src, _, _, _),
+    not(airport(Dest, _, _, _)),
+    write('Destination airport does not exist'), nl, !, fail.
+
+fly(Src, Dest) :-
+    not(airport(Src, _, _, _)),
+    not(airport(Dest, _, _, _)),
+    write('Source and destination airports do not exist'), nl, !, fail.
